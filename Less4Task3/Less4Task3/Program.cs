6ca7d72b82ee76c0f44391676c69c27fbde057cd
Ruslantahirov <ruslantahirov42@gmail.com>
2022-06-16 20:24:50 +0400

    Change

---
+++ Less4Task3/Less4Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Less4Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (0<age && age<30)
+            {
+               
+                Console.WriteLine(age*age);
+            }
+            else if (30<age && age<40)
+            {
+                int x = age % 10;
+                Console.WriteLine(x);
+            }
+
+            else if (age<0 || age>100)
+            {
+                Console.WriteLine("Düzgün məlumat daxil etməmisiniz");
+            }
+
+        }
+    }
+}
---
+++ Less5Task1/Less5Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Less5Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 6, 7, 8, 23, 78, 34, 12 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+
+            Console.WriteLine("-----------");
+
+
+            int min = nums.Min();
+            Console.WriteLine(min);//KIcik eded
+
+            Console.WriteLine("-----------");
+
+            Console.WriteLine(nums.Max());//Boyuk eded
+
+            Console.WriteLine("-----------");
+
+            Array.Sort(nums);
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);  //Sortlasdirmaq
+            }
+
+            Console.WriteLine("-----------");
+
+            Console.WriteLine(nums.Sum()); //Cemi
+
+            Console.WriteLine("-----------");
+
+
+
+
+        }
+    }
+}
